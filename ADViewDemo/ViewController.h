02121ed3e480ae3cf051d467453a7e54c2cf4842
李峰 <lifeng@lifengdemacbook-pro.local>
2018-03-07 09:35:41 +0800

    Initial Commit

---
+++ ADViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADViewDemo
+//
+//  Created by 李峰 on 2018/3/7.
+//  Copyright © 2018年 Stward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ADViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ADViewDemo
+//
+//  Created by 李峰 on 2018/3/7.
+//  Copyright © 2018年 Stward. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
